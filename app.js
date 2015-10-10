d48e80d77d1428b7c465633ff07e4a9a8b8ddb71
joshmmo <josh.moore@infusionsoft.com>
2015-10-10 15:10:43 -0700

    run ifconfig command on a new line

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ var value = pin13.read();
 if (value == 1) {
     console.log("Wifi Enabled");
 } else {
-    var ssid = "eRobot" + randomInt(1, 1000000).toString(16);
+    var ssid = "eRobot-" + randomInt(1, 10000000).toString(16);
     var ip = "192.168.1.2";
     exec("sh wpacli_ibss_open.sh "+ssid + "&& ifconfig wlan0 " + ip, puts);
-    console.log("Ad-Hoc Enabled. IP Of this machine: " + ip);
+    console.log("Ad-Hoc Enabled.");
+    exec("ifconfig wlan0 " + ip, puts);
+    console.log("IP: " + ip);
 }
 
 function randomInt(low, high) {
