d7715aa58a9fd091e2f4323de6f6519add312041
joshmmo <josh.moore@infusionsoft.com>
2015-10-10 15:26:07 -0700

    add timer

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ var exec = require('child_process').exec;
 var pin13 = new mraa.Gpio(13);
 pin13.dir(mraa.DIR_IN);
 
-function puts(error, stdout, stderr) {
-    sys.puts(stdout);
-    sys.puts(error);
-    sys.puts(stderr);
+function log(error, stdout, stderr) {
+    console.log(stdout);
 }
 
 
@@ -18,9 +16,13 @@ if (value == 1) {
 } else {
     var ssid = "eRobot-" + randomInt(1, 10000000).toString(16);
     var ip = "192.168.1.2";
-    exec("sh wpacli_ibss_open.sh "+ssid + "&& ifconfig wlan0 " + ip, puts);
+    exec("sh wpacli_ibss_open.sh "+ssid + "&& ifconfig wlan0 " + ip, log);
     console.log("Ad-Hoc Enabled.");
-    exec("ifconfig wlan0 " + ip, puts);
+
+    setTimeout(function() {
+        exec("ifconfig wlan0 " + ip, log);
+    }, 2000);
+
     console.log("IP: " + ip);
 }
 
