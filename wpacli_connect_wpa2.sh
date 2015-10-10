dd21d9c8b857b16a2991f92593d8a1dfe89838fe
joshmmo <josh.moore@infusionsoft.com>
2015-10-10 00:33:27 -0700

    updated adhoc script

--- wpacli_connect_wpa2.sh
+++ wpacli_connect_wpa2.sh
@@ -1,18 +1,20 @@
-if [ $# != 2 ] ; then
- echo "$0 <SSID> <passphrase>"
+if [ $# != 1 ] ; then
+ echo "$0 <SSID>"
  exit
 fi
-wpa_cli -iwlan0 disconnect
-wpa_cli -iwlan0 remove_network all
-wpa_cli -iwlan0 add_network
-wpa_cli -iwlan0 set_network 0 mode 0
-wpa_cli -iwlan0 set_network 0 ssid \"$1\"
-wpa_cli -iwlan0 set_network 0 auth_alg OPEN
-wpa_cli -iwlan0 set_network 0 key_mgmt WPA-PSK
-wpa_cli -iwlan0 set_network 0 proto RSN
-wpa_cli -iwlan0 set_network 0 psk \"$2\"
-wpa_cli -iwlan0 set_network 0 scan_ssid 1
-wpa_cli -iwlan0 select_network 0
-wpa_cli -iwlan0 enable_network 0
-wpa_cli -iwlan0 reassociate
-wpa_cli -iwlan0 status
+$rmt wpa_cli -iwlan0 disconnect
+$rmt wpa_cli -iwlan0 remove_network all
+$rmt wpa_cli -iwlan0 add_network
+$rmt wpa_cli -iwlan0 set_network 0 frequency 2412
+$rmt wpa_cli -iwlan0 set_network 0 mode 1
+if [ ! -n "$rmt" ] ; then
+$rmt wpa_cli -iwlan0 set_network 0 ssid \"$1\"
+else
+$rmt wpa_cli -iwlan0 set_network 0 ssid '\"'$1'\"'
+fi
+$rmt wpa_cli -iwlan0 set_network 0 auth_alg OPEN
+$rmt wpa_cli -iwlan0 set_network 0 key_mgmt NONE
+$rmt wpa_cli -iwlan0 set_network 0 scan_ssid 1
+$rmt wpa_cli -iwlan0 select_network 0
+$rmt wpa_cli -iwlan0 enable_network 0
+$rmt wpa_cli -iwlan0 status
