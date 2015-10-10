f40833a5828ee70b3606fd95c4954e2f4bb26333
joshmmo <josh.moore@infusionsoft.com>
2015-10-09 23:50:39 -0700

    added infinite loop

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var mraa = require('mraa');
+var InfiniteLoop = require('infinite-loop');
 
-var pin13 = new mraa.Gpio(13);
-pin13.dir(mraa.DIR_IN);
-var value = pin13.read();
-console.log(value);
+var il = new InfiniteLoop;
+
+//simple ++ counter example
+var counter = 0;
+//task you want to run infinitely
+function addOne() {
+    counter++;
+    console.log(counter);
+}
+
+//add it by calling .add
+il.add(addOne, []);
+
+il.run();
+
+//var pin13 = new mraa.Gpio(13);
+//pin13.dir(mraa.DIR_IN);
+//var value = pin13.read();
+//console.log(value);
