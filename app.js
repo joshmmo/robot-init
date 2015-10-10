9ad333eba92033ee5444514daee7cd405671246c
joshmmo <josh.moore@infusionsoft.com>
2015-10-10 00:22:16 -0700

    removed infintie loop. Added sys commands

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var mraa = require('mraa');
-var InfiniteLoop = require('infinite-loop');
+var sys = require('sys');
+var exec = require('child_process').exec;
 
-var il = new InfiniteLoop;
-var enableWifi = true;
 var pin13 = new mraa.Gpio(13);
 pin13.dir(mraa.DIR_IN);
 
-function checkNetworkSwitch() {
-    var value = pin13.read();
-    if (value == 1) {
-        console.log("Wifi Enabled");
-    } else {
-        console.log("Ad-Hoc Enabled");
-    }
+var child;
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("ls -la", puts);
+
+var value = pin13.read();
+if (value == 1) {
+    console.log("Wifi Enabled");
+} else {
+    console.log("Ad-Hoc Enabled");
 }
-
-il.add(checkNetworkSwitch, []);
-
-il.run();
